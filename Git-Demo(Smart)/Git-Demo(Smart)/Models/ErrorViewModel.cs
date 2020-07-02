684686177e0535dcc26ed0326079d04cbc7c0e32
megha.i.garg <megha.i.garg@avanade.com>
2020-07-02 11:13:47 +0200

    my try final

---
+++ Git-Demo(Smart)/Git-Demo(Smart)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Git_Demo_Smart_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
